2b2921d1b5c7563dd48fb88f000a0ff86d529106
Anvar <mail@anvar.in>
2018-09-23 14:15:53 +0400

    Revex Design

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>RevX - Complete review of your website</title>
+    <link rel="stylesheet" href="src/css/custom.css">
+    <link rel="stylesheet" href="src/css/mobile.css">
+    <script src="src/js/all.js"></script>
+    <script src="src/js/jquery.min.js"></script>
+    <script src="src/js//typed.custom.js" type="text/javascript"></script>
+    <script src="src/js/script.js" type="text/javascript"></script>
+  </head>
+  <body>
+
+      <h1 id="typed"></h1>
+      <div class="wrap">
+         <div class="search">
+            <input type="text" class="searchTerm" placeholder="Enter your website">
+            <button type="submit" class="searchButton">
+              <i class="fa fa-search"></i>
+           </button>
+         </div>
+      </div>
+
+
+      <div class="f-smd">
+        <a class="f-sm" href="https://facebook.com/teamxtec"><i class="fab fa-facebook fa-2x"></i></a>
+        <a class="f-sm" href="https://twitter.com/teamxtec"><i class="fab fa-twitter-square fa-2x"></i></a>
+        <a  class="f-sm" href="https://instagram.com/teamxtec"><i class="fab fa-instagram fa-2x"></i></a>
+        <a  class="f-sm" href="https://linkedin.com/company/teamxtec"><i class="fab fa-linkedin fa-2x"></i></a>
+        <a  class="f-sm" href="https://t.me/teamxtec"><i class="fab fa-telegram fa-2x" ></i></a>
+      </div>
+  </body>
+</html>
---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+$(function(){
+
+  $("#typed").typed({
+    strings: ["Welcome to ReveX", "Check your website", "Technology for all", "TeamX IT Solutions"],
+    typeSpeed: 100,
+    callback: function(){
+      shift();
+    }
+  });
+
+});
+function shift(){
+    $(".head-wrap").addClass("shift-text");
+    terminalHeight();
+}
+
+function terminalHeight(){
+    var termHeight = $(".terminal-height");
+    var value = termHeight.text();
+    value = parseInt(value);
+    setTimeout(function(){
+        if (value > 10){
+            value = value-1;
+            this.txtValue = value.toString();
+            termHeight.text(this.txtValue);
+            self.terminalHeight();
+        }
+        else{
+            clearTimeout();
+        }
+    }, 10);
+}
